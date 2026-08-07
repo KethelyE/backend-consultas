8c2d351745d15b1d5c1f8895e1a818446ed8e034
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-07 09:38:59 -0300

    feat: adiciona DataLoader para criar dados iniciais automaticamente e adiciona método setId na classe Paciente

--- src/main/java/com/fiap/ec/backend_consultas/repository/ConsultaRepository.java
+++ src/main/java/com/fiap/ec/backend_consultas/repository/ConsultaRepository.java
@@ -1,9 +1,10 @@
 package com.fiap.ec.backend_consultas.repository;
 
-import com.fiap.ec.backend_consultas.model.Consulta;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import com.fiap.ec.backend_consultas.model.Consulta;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
     List<Consulta> findByMedicoId(Long medicoId);
